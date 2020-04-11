60d43abe53d8d7e49684e8149fe8f14c3b3e294e
BenedyktT <benedykt.tyminski@gmail.com>
2020-04-11 14:09:59 +0000

    setup test refactor'

---
+++ tests/setup.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+require("../models/User");
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+/* const connectMemoryDb = async () => {
+  const keys = require("../config/keys");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongod = new MongoMemoryServer();
+  const uri = await mongod.getConnectionString();
+
+  const mongooseOpts = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  };
+  await mongoose.connect(uri, mongooseOpts);
+};
+ */
